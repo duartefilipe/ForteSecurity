bf0fbc7340e412c6ac0e2ad0e66905b7b3c0a9ea
Maico Camargo <maico.silva@meta.com.br>
2021-06-22 23:40:35 -0300

    add routes

--- forteapi/src/application/routes/routes.js
+++ forteapi/src/application/routes/routes.js
@@ -1,56 +1,27 @@
-const express = require('express');
-const bodyparse = require('body-parser');
-const usuario = require('./database/models/usuario');
-const empresa = require('./database/models/empresa');
-const lugar = require('./database/models/lugar');
-const inventario = require('./database/models/inventario');
-const path = require('path');
-const app = express();
-app.use('/imagens', express.static(path.join(__dirname, '/imagens')));
-const bcrypt = require('bcrypt');
-const cors = require('cors');
-const session = require('express-session');
-const multer = require('multer');
-app.use(bodyparse.urlencoded({extended:false}));
-app.use(bodyparse.json());
+import express from "express";
 
-app.use(session({
-    secret:'keyboard cat',
-    name: 'essecookie',
-    proxy: true,
-    resave: true,
-    saveUninitialized: true
-}))
+const usuario = require('../../infrastructure/database/models/usuario');
+const empresa = require('../../infrastructure/database/models/empresa');
+const lugar = require('../../infrastructure/database/models/lugar');
+const inventario = require('../../infrastructure/database/models/inventario');
+import multer from 'multer'
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "imagens/")
+        cb(null, "imagens/")
     },
     filename: (req, file, cb) => {
-      cb(null, Date.now() + "-" + file.ogirinalname + ".jpg")
+        cb(null, Date.now() + "-" + file.ogirinalname + ".jpg")
     },
-  })
-
+})
 const upload = multer({ storage })
+const routes = express.Router()
 
-const porta = 8080;
-
-app.use(cors());
-
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
-
-app.get('/', (req, res) =>{
+routes.get('/', (req, res) =>{
     res.send('Api do Curso');
 });
 
-app.get('/returnImg/:idUsu', (req,res)=>{
+routes.get('/returnImg/:idUsu', (req,res)=>{
     const idUsu=req.params.idUsu;
     usuario.findOne({where:{idUsu:idUsu}}).then(result =>{
         if(result != undefined){
@@ -61,7 +32,7 @@ app.get('/returnImg/:idUsu', (req,res)=>{
     })
 })
 
-app.post('/uploadImg', upload.single('file'), (req, res)=>{
+routes.post('/uploadImg', upload.single('file'), (req, res)=>{
     const idUsu= req.body.idUsu;
     const senha = req.body.senha;
     //essa parte da senha é por questao de segurança para não ser possivel alterar a foto do usuario sem estar logado
@@ -76,7 +47,7 @@ app.post('/uploadImg', upload.single('file'), (req, res)=>{
     })
 })
 
-app.post('/updatePerfil', (req, res)=>{
+routes.post('/updatePerfil', (req, res)=>{
     const idUsu= req.body.idUsu;
     const email = req.body.email;
     const nome = req.body.nome;
@@ -95,7 +66,7 @@ app.post('/updatePerfil', (req, res)=>{
     })
 })
 
-app.post('/registrar', function(req, res){
+routes.post('/registrar', function(req, res){
     const nome =req.body.nome;
     const senha =req.body.senha;
     const email =req.body.email;
@@ -117,24 +88,24 @@ app.post('/registrar', function(req, res){
         }})
 })
 
-app.post('/criarEmpresa', upload.single('file'), (req, res) => {
+routes.post('/criarEmpresa', upload.single('file'), (req, res) => {
     const razaosocial = req.body.razaosocial;
     const cnpj = req.body.cnpj;
     const email = req.body.email;
     const perfil = req.body.perfil;
     const username = req.body.username;
 
-        empresa.create({
-            razaosocial:razaosocial,
-            cnpj:cnpj,
-            email:email,
-            perfil:perfil,
-            username:username
+    empresa.create({
+        razaosocial:razaosocial,
+        cnpj:cnpj,
+        email:email,
+        perfil:perfil,
+        username:username
 
-        }).then(() =>{return res.json("Sucesso ao gravar")})
-})  
+    }).then(() =>{return res.json("Sucesso ao gravar")})
+})
 
-app.post('/criarLugar', upload.single('file'), (req, res) => {
+routes.post('/criarLugar', upload.single('file'), (req, res) => {
     const lugarNome = req.body.lugarNome;
     const equipe = req.body.equipe;
     const responsavel = req.body.responsavel;
@@ -143,59 +114,59 @@ app.post('/criarLugar', upload.single('file'), (req, res) => {
     const fonte_acesso = req.body.fonte_acesso;
     const idEmp = req.body.idEmp;
 
-        lugar.create({
-            lugarNome:lugarNome,
-            equipe:equipe,
-            responsavel:responsavel,
-            email:email,
-            perfis:perfis,
-            fonte_acesso:fonte_acesso,
-            idEmp:idEmp
+    lugar.create({
+        lugarNome:lugarNome,
+        equipe:equipe,
+        responsavel:responsavel,
+        email:email,
+        perfis:perfis,
+        fonte_acesso:fonte_acesso,
+        idEmp:idEmp
 
-        }).then(() =>{return res.json("Sucesso ao gravar lugar")})
-})  
+    }).then(() =>{return res.json("Sucesso ao gravar lugar")})
+})
 
-app.get('/empresas/:idEmp', (req,res)=>{
+routes.get('/empresas/:idEmp', (req,res)=>{
     const idEmp = req.params.idEmp;
     empresa.findOne({where:{idEmp:idEmp}}).then(result =>{
         res.json(result)
     })
 })
 
-app.post('/deleteLugar/:idLug', (req,res)=>{
+routes.post('/deleteLugar/:idLug', (req,res)=>{
     const idLug= req.params.idLug;
     lugar.destroy({where: { idLug: idLug}})
-      .then(console.log('deletado inventario'));
-      return res.json(1);
+        .then(console.log('deletado inventario'));
+    return res.json(1);
 })
 
-app.get('/empresasAll', (req,res)=>{
+routes.get('/empresasAll', (req,res)=>{
     empresa.findAll().then(result =>{
         res.json(result)
     })
 })
 
-app.get('/lugares/:idEmp', (req,res)=>{
+routes.get('/lugares/:idEmp', (req,res)=>{
     const idEmp = req.params.idEmp;
     lugar.findAll({where:{idEmp:idEmp}}).then(result =>{
         res.json(result)
     })
 })
 
-app.get('/inventario/:idLug', (req,res)=>{
+routes.get('/inventario/:idLug', (req,res)=>{
     const idLug = req.params.idLug;
     inventario.findAll({where:{idLug:idLug}}).then(result =>{
         res.json(result)
     })
 })
 
-app.get('/inventarioAll', (req,res)=>{
+routes.get('/inventarioAll', (req,res)=>{
     inventario.findAll().then(result =>{
         res.json(result)
     })
 })
 
-app.post('/login', (req,res)=>{
+routes.post('/login', (req,res)=>{
     const email = req.body.email;
     const senha = req.body.senha;
 
@@ -203,18 +174,18 @@ app.post('/login', (req,res)=>{
         var verify = bcrypt.compareSync(senha,result.senha);
 
         if(verify){
-           
+
             req.session.result = {
                 idUsu: result.idUsu,
                 nome: result.nome,
                 email: result.email,
                 imagem: result.imagem,
                 perfil: result.perfil,
-               // senha: result.senha
-            
+                // senha: result.senha
+
             }
-            
-            
+
+
             return res.send(req.session.result)
         }else{
             return res.json(1)
@@ -222,6 +193,4 @@ app.post('/login', (req,res)=>{
     })
 })
 
-app.listen(porta, () =>{
-    console.log('listen porta: '+porta);
-});
+export default routes
